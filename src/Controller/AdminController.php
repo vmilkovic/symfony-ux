7ecad17698da972f3eb6190c5de9d5baa62600dd
Vedran Milković <vedran.milkovic25@gmail.com>
2021-04-29 21:59:20 +0200

    Stimulus with Chart.js and React

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,14 +4,40 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function dashboard()
+    public function dashboard(ChartBuilderInterface $chartBuilder)
     {
-        return $this->render('admin/dashboard.html.twig');
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label'             => 'My First dataset',
+                    'backgroundColor'   => 'rgb(255, 99, 132)',
+                    'borderColor'       => 'rgb(255, 99, 132)',
+                    'data'              => [0, 10, 5, 2, 20, 30, 45],
+                ],
+            ],
+        ]);
+        
+        $chart->setOptions([
+            'scales' => [
+                'yAxes' => [[
+                    'ticks' => [
+                        'beginAtZero' => true
+                    ]
+                ]]
+            ]
+        ]);
+
+        return $this->render('admin/dashboard.html.twig', [
+            'chart' => $chart,
+        ]);
     }
 }
