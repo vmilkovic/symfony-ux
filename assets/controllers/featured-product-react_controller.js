32115ba70a6010753413802829ed2d8eb84cc1d7
Vedran Milković <vedran.milkovic25@gmail.com>
2021-04-30 21:39:40 +0200

    Async/Dynamic import

--- assets/controllers/featured-product-react_controller.js
+++ assets/controllers/featured-product-react_controller.js
@@ -1,12 +1,16 @@
 import { Controller } from 'stimulus';
 import React from 'react';
-import MadeWithLove from '../components/MadeWithLove';
+import FeatureProduct from '../components/FeatureProduct';
 
 export default class extends Controller {
+    static values = {
+        product: Object
+    }
+
     connect() {
         import('react-dom').then((ReactDOM) => {
             ReactDOM.default.render(
-                <MadeWithLove />,
+                <FeatureProduct product={this.productValue} />,
                 this.element
             )
         })
